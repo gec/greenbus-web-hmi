cad539570d13ba1d61ad10c7feb25954ef604456
Flint O'Brien <fobrien@greenenergycorp.com>
2015-03-09 15:07:06 -0400

    fix(measurement): Command and setpoint working again.

--- public/lib/greenbus-views/greenbus-views-tpls-0.1.0-SNAPSHOT.js
+++ public/lib/greenbus-views/greenbus-views-tpls-0.1.0-SNAPSHOT.js
@@ -2,11 +2,11 @@
  * greenbus-web-views
  * https://github.com/gec/greenbus-web-views
 
- * Version: 0.1.0-SNAPSHOT - 2015-03-03
+ * Version: 0.1.0-SNAPSHOT - 2015-03-09
  * License: Apache Version 2.0
  */
-angular.module("greenbus.views", ["greenbus.views.tpls", "greenbus.views.authentication","greenbus.views.chart","greenbus.views.endpoint","greenbus.views.ess","greenbus.views.event","greenbus.views.measurement","greenbus.views.navigation","greenbus.views.notification","greenbus.views.request","greenbus.views.rest","greenbus.views.selection","greenbus.views.subscription"]);
-angular.module("greenbus.views.tpls", ["greenbus.views.template/chart/chart.html","greenbus.views.template/chart/charts.html","greenbus.views.template/endpoint/endpoints.html","greenbus.views.template/ess/esses.html","greenbus.views.template/event/alarms.html","greenbus.views.template/event/alarmsAndEvents.html","greenbus.views.template/event/events.html","greenbus.views.template/measurement/measurements.html","greenbus.views.template/navigation/navBarTop.html","greenbus.views.template/navigation/navList.html","greenbus.views.template/notification/notification.html","greenbus.views.template/selection/selectAll.html"]);
+angular.module("greenbus.views", ["greenbus.views.tpls", "greenbus.views.authentication","greenbus.views.chart","greenbus.views.endpoint","greenbus.views.ess","greenbus.views.event","greenbus.views.measurement","greenbus.views.measurementValue","greenbus.views.navigation","greenbus.views.notification","greenbus.views.request","greenbus.views.rest","greenbus.views.selection","greenbus.views.subscription"]);
+angular.module("greenbus.views.tpls", ["greenbus.views.template/chart/chart.html","greenbus.views.template/chart/charts.html","greenbus.views.template/endpoint/endpoints.html","greenbus.views.template/ess/esses.html","greenbus.views.template/event/alarms.html","greenbus.views.template/event/alarmsAndEvents.html","greenbus.views.template/event/events.html","greenbus.views.template/measurement/measurements.html","greenbus.views.template/measurementValue/measurementValue.html","greenbus.views.template/navigation/navBarTop.html","greenbus.views.template/navigation/navList.html","greenbus.views.template/notification/notification.html","greenbus.views.template/selection/selectAll.html"]);
 /**
 * Copyright 2013-2014 Green Energy Corp.
 *
@@ -3262,7 +3262,7 @@ CommandSet.prototype.select = function( command) {
         console.log( 'commandLock delay: ' + delay)
         // It the clock for client vs server is off, we'll use a minimum delay.
         delay = Math.max( delay, 10)
-        self.selectTimeout = this.timeout(function () {
+        self.selectTimeout = self.timeout(function () {
           delete self.lock;
           delete self.selectTimeout;
           if( self.state === CommandSet.States.Selected || self.state === CommandSet.States.Executing) {
@@ -3364,7 +3364,7 @@ CommandSet.prototype.execute = function( command, commandIndex) {
   self.setState( CommandSet.States.Executing, command)
 
 
-  self.commandRest.execute( command.id,
+  self.commandRest.execute( command.id, args,
     function( commandResult) {
       self.alertCommandResult( commandResult)
       self.deselected()
@@ -4042,8 +4042,8 @@ angular.module('greenbus.views.measurement', ['greenbus.views.subscription', 'gr
         deselect: function( lockId, success, failure) {
           rest.delete( '/models/1/commandlock/' + lockId, null, $scope, success, failure)
         },
-        execute: function( commandId, success, failure) {
-          rest.post( '/models/1/commands/' + commandId, success, failure)
+        execute: function( commandId, args, success, failure) {
+          rest.post( '/models/1/commands/' + commandId, args, null, $scope, success, failure)
         }
       }
 
@@ -4144,7 +4144,7 @@ angular.module('greenbus.views.measurement', ['greenbus.views.subscription', 'gr
         image = '../../images/pointRaw.png'
       } else {
         switch( type) {
-          case 'ANALOG': image = '/images/pointAnalog.png'; break;
+          case 'ANALOGANALOG': image = '/images/pointAnalog.png'; break;
           case 'STATUS': image = '/images/pointStatus.png'; break;
           default: image = '/images/pointRaw.png';
         }
@@ -4168,6 +4168,281 @@ angular.module('greenbus.views.measurement', ['greenbus.views.subscription', 'gr
       }
 
       return text
+    };
+  })
+
+
+
+/**
+ * Copyright 2014-2015 Green Energy Corp.
+ *
+ * Licensed to Green Energy Corp (www.greenenergycorp.com) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. Green Energy
+ * Corp licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Author: Flint O'Brien
+ */
+
+
+angular.module('greenbus.views.measurementValue', []).
+
+  factory('gbMeasurementValueRest', ['rest', function( rest) {
+
+    /**
+     * Override measurements for a point.
+     *
+     * @param pointId Point ID to be overridden
+     * @param value The new value as a string
+     * @param valueType The type of the value to override. BOOL, INT, DOUBLE, STRING
+     * @param success Success callback
+     * @param failure Failure callback
+     */
+    function override( pointId, value, valueType, callee, success, failure) {
+
+      var arg = {
+            value: value,
+            valueType: valueType
+          },
+          url = '/models/1/points/' + pointId + '/override'
+
+      rest.post( url, arg, null, null,
+        function( data) {
+          success.call( callee, data)
+        },
+        function( ex, statusCode, headers, config){
+          console.log( 'gbMeasurementValueRest ERROR overriding point with ID: ' + pointId + ' to value "' + value + '" with type: "' + valueType + '". Exception: ' + ex.exception + ' - ' + ex.message)
+          failure.call( callee, pointId, value, valueType, ex, statusCode)
+        }
+      )
+
+      return true
+    }
+
+    /**
+     * Put a point NIS (Not In Service)
+     *
+     * @param pointId Point ID to be overridden
+     */
+    function nis( pointId, callee, success, failure) {
+
+      var arg,
+          url = '/models/1/points/' + pointId + '/nis'
+
+      rest.post( url, arg, null, null,
+        function( data) {
+          success.call( callee, data)
+        },
+        function( ex, statusCode, headers, config){
+          console.log( 'gbMeasurementValueRest ERROR NIS point with ID: ' + pointId + '. Exception: ' + ex.exception + ' - ' + ex.message)
+          failure.call( callee, pointId, ex, statusCode)
+        }
+      )
+
+      return true
+    }
+
+    /**
+     * Remove a point's override or NIS (Not In Service)
+     *
+     * @param pointId Point ID to remove the override or NIS.
+     */
+    function remove( pointId, callee, success, failure, nisOrOverride) {
+
+      var arg,
+          url = '/models/1/points/' + pointId + '/' + nisOrOverride
+
+      rest.delete( url, arg, null, null,
+        function( data) {
+          success.call( callee, data)
+        },
+        function( ex, statusCode, headers, config){
+          console.log( 'gbMeasurementValueRest ERROR NIS point with ID: ' + pointId + '. Exception: ' + ex.exception + ' - ' + ex.message)
+          failure.call( callee, pointId, ex, statusCode)
+        }
+      )
+
+      return true
+    }
+
+    function removeNis( pointId, callee, success, failure) { remove( pointId, callee, success, failure, 'nis')}
+    function removeOverride( pointId, callee, success, failure) { remove( pointId, callee, success, failure, 'override')}
+
+
+    /**
+     * Public API
+     */
+    return {
+      override: override,
+      nis: nis,
+      removeNis: removeNis,
+      removeOverride: removeOverride
+    }
+  }]).
+
+  controller( 'gbMeasurementValueController', ['$scope', 'gbMeasurementValueRest', function( $scope, gbMeasurementValueRest) {
+    var self = this
+    $scope.editMode = undefined // {value: '', valueType: ''}
+    $scope.canRemove = false
+    $scope.canNis = false
+    $scope.removeTooltip = undefined // Remove NIS, Remove Replace
+    $scope.placeHolder = ''
+    $scope.requestPending = undefined
+    $scope.requestError = undefined
+
+
+    self.configureInput = function() {
+      var m = $scope.model.currentMeasurement
+      switch( m.shortQuality) {
+        case 'R':
+          $scope.canRemove = true
+          $scope.canNis = true
+          $scope.removeTooltip = 'Remove replace'
+          break
+        case 'N':
+          $scope.canRemove = true
+          $scope.canNis = false
+          $scope.removeTooltip = 'Remove NIS'
+          break
+        default:
+          $scope.canRemove = false
+          $scope.canNis = true
+          $scope.removeTooltip = undefined
+          break
+      }
+
+      // set focus and select text
+    }
+
+    self.getRemoveTooltip = function() {
+      var m = $scope.model.currentMeasurement
+      switch( m.shortQuality) {
+        case 'R': return $scope.removeTooltip = 'Remove replace'
+        case 'N': return $scope.removeTooltip = 'Remove NIS'
+        default: return $scope.removeTooltip = undefined
+      }
+    }
+
+    function requesting( requestType ) {
+      $scope.requestError = undefined
+      $scope.requestPending = requestType
+    }
+    $scope.nis = function() {
+      var m = $scope.model.currentMeasurement
+      if( m.shortQuality !== 'R' && m.shortQuality !== 'N') {
+        requesting( 'nis')
+        gbMeasurementValueRest.nis(
+          $scope.model.id,
+          this,
+          function( ) {
+
+          },
+          function( ) {
+
+          }
+        )
+      }
+    }
+    $scope.replace = function() {
+      var m = $scope.model.currentMeasurement
+      requesting( 'nis')
+      gbMeasurementValueRest.override(
+        $scope.model.id,
+        $scope.editMode.value,
+        $scope.editMode.valueType,
+        this,
+        function( ) {
+
+        },
+        function( ) {
+
+        }
+      )
+    }
+    $scope.remove = function() {
+      var m = $scope.model.currentMeasurement
+      if( m.shortQuality === 'R' || m.shortQuality === 'N') {
+
+      }
+
+    }
+    $scope.onBlur = function() {
+
+    }
+    $scope.onFocus = function() {
+
+    }
+
+    $scope.edit = function() {
+      if( $scope.editMode)
+        return
+
+      $scope.editMode = {
+        value: $scope.model.currentMeasurement.value
+      }
+      $scope.removeTooltip = self.getRemoveTooltip()
+    }
+
+  }]).
+
+  directive( 'gbMeasurementValue', function(){
+    return {
+      restrict: 'E', // Element name
+      // The template HTML will replace the directive.
+      replace: true,
+      scope: {
+             model  : '=',
+             notify: '&'
+      },
+      templateUrl: 'greenbus.views.template/measurementValue/measurementValue.html',
+      controller: 'gbMeasurementValueController',
+      link: function(scope, element, attrs, controller) {
+        var focusedElement
+        element.on('click', function () {
+          if ( ! scope.editMode) {
+            scope.edit()
+            scope.$digest()
+
+            this.select();
+            focusedElement = this;
+          }
+        })
+        element.on('blur', function () {
+          focusedElement = null;
+        })
+
+        var selectItem = attrs.selectItem || 'selectItem'
+        scope.$parent[selectItem] = controller.selectItem
+        scope.$parent.uncheckItem = controller.uncheckItem
+        controller.notifyParent = function( state) {
+          scope.notify( {state: state})
+        }
+      }
+    }
+  }).
+
+  filter('measurementValueRemoveClass', function() {
+    return function(state, updateState) {
+      var s
+      switch( state) {
+        case 'ACKNOWLEDGED': s = 'fa fa-trash gb-alarm-remove'; break;
+        case 'REMOVED': s = 'fa fa-trash-o  gb-alarm-ack'; break;
+        default: s = 'fa'; break;
+      }
+
+      if( updateState === 'removing')
+        s += ' fa-spin'
+      return s
     };
   })
 
@@ -6053,6 +6328,32 @@ angular.module("greenbus.views.template/measurement/measurements.html", []).run(
     "    <!--</div>-->\n" +
     "</div>\n" +
     "\n" +
+    "");
+}]);
+
+angular.module("greenbus.views.template/measurementValue/measurementValue.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("greenbus.views.template/measurementValue/measurementValue.html",
+    "<span>\n" +
+    "    <span ng-if=\"!editMode\" ng-click=\"edit()\">{{ model.currentMeasurement.value }}</span>\n" +
+    "\n" +
+    "    <div ng-if=\"editMode\">\n" +
+    "        <input type=\"text\" class=\"form-control\" ng-model=\"editMode.value\" ng-onblur=\"onBlur()\" ng-focus=\"onFocus()\" name=\"measurement_value\" ng-pattern=\"\" style=\"width:6em;\" placeholder=\"{{ placeHolder}}\">\n" +
+    "        <div ng-if=\"model.currentMeasurement.shortQuality==='R'\" class=\"btn-group\" role=\"group\" aria-label=\"...\">\n" +
+    "            <button ng-click=\"replace()\" type=\"button\" class=\"btn btn-info\" popover=\"Replace\" popover-trigger=\"mouseenter\" popover-placement=\"bottom\">R</button>\n" +
+    "            <button ng-click=\"nis()\"     type=\"button\" class=\"btn btn-info\" popover=\"Not In Service\" popover-trigger=\"mouseenter\" popover-placement=\"bottom\">N</button>\n" +
+    "        </div>\n" +
+    "        <div ng-if=\"model.currentMeasurement.shortQuality==='N'\" class=\"btn-group\" role=\"group\" aria-label=\"...\">\n" +
+    "            <button ng-click=\"replace()\" type=\"button\" class=\"btn btn-info\" popover=\"Replace\" popover-trigger=\"mouseenter\" popover-placement=\"bottom\">R</button>\n" +
+    "            <button ng-click=\"nis()\"     type=\"button\" class=\"btn btn-info disabled\" popover=\"Not In Service\" popover-trigger=\"mouseenter\" popover-placement=\"bottom\">N</button>\n" +
+    "            <button ng-click=\"remove()\"  type=\"button\" class=\"btn btn-danger\" popover=\"{{removeTooltip}}\" popover-trigger=\"mouseenter\" popover-placement=\"bottom\"><i class=\"fa fa-minus\"></i></button>\n" +
+    "        </div>\n" +
+    "        <div ng-if=\"model.currentMeasurement.shortQuality!=='N' && model.currentMeasurement.shortQuality!=='R'\" class=\"btn-group\" role=\"group\" aria-label=\"...\">\n" +
+    "            <button ng-click=\"replace()\" type=\"button\" class=\"btn btn-info\" popover=\"Replace\" popover-trigger=\"mouseenter\" popover-placement=\"bottom\">R</button>\n" +
+    "            <button ng-click=\"nis()\"     type=\"button\" class=\"btn btn-info\" popover=\"Not In Service\" popover-trigger=\"mouseenter\" popover-placement=\"bottom\">N</button>\n" +
+    "        </div>\n" +
+    "        <a ng-if=\"model.currentMeasurement.shortQuality==='N' || model.currentMeasurement.shortQuality==='R'\" ng-click=\"remove()\"  type=\"button\" class=\"text-danger gb-remove\" popover=\"{{removeTooltip}}\" popover-trigger=\"mouseenter\" popover-placement=\"bottom\"><i class=\"fa fa-minus-circle\"></i></a>\n" +
+    "    </div>\n" +
+    "</span>\n" +
     "");
 }]);
 
